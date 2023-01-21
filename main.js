c4f9c61c403d3c9334b0ac66ba7ba72b5043baa9
Darkolas091 <Darkolas091@users.noreply.github.com>
2023-01-21 14:42:09 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+    // Swiper
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  
+  //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
