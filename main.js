f2dfd052a9b62a6a61b95fbe53ad5223b7b6084b
Darkolas091 <Darkolas091@users.noreply.github.com>
2023-01-25 22:26:43 +0100

    categories
    attempted to hide with JS

--- main.js
+++ main.js
@@ -27,14 +27,75 @@ window.onscroll = () => {
   navbar.classList.remove('active');
 }
 
-document.querySelectorAll('.box').forEach(box => {
-  box.addEventListener('click', function() {
-      document.querySelectorAll('.box').forEach(box => {
-          box.style.borderColor = "transparent";
-          box.style.border = "none";
-      });
-      this.style.borderColor = "black";
-      this.style.border = "2px solid";
-      this.style.transition = "none";
+
+
+document.querySelector('.box1').addEventListener('click', function() {
+  document.querySelectorAll('.box1, .box2, .box3, .box4, .box5').forEach(box => {
+      box.style.borderColor = "transparent";
+      box.style.border = "none";
   });
+  this.style.borderColor = "black";
+  this.style.border = "2px solid";
+  this.style.transition = "none";
+  document.querySelector('.ostalo').style.display = 'none';
+  document.querySelector('.povrce').style.display = 'none';
+  document.querySelector('.or').style.display = 'none';
+  document.querySelector('.zac').style.display = 'none';
 });
+
+document.querySelector('.box2').addEventListener('click', function() {
+  document.querySelectorAll('.box1, .box2, .box3, .box4, .box5').forEach(box => {
+      box.style.borderColor = "transparent";
+      box.style.border = "none";
+  });
+  this.style.borderColor = "black";
+  this.style.border = "2px solid";
+  this.style.transition = "none";
+  document.querySelector('.ostalo').style.display = 'block';
+  document.querySelector('.povrce').style.display = 'none';
+  document.querySelector('.or').style.display = 'none';
+  document.querySelector('.zac').style.display = 'none';
+});
+
+document.querySelector('.box3').addEventListener('click', function() {
+  document.querySelectorAll('.box1, .box2, .box3, .box4, .box5').forEach(box => {
+      box.style.borderColor = "transparent";
+      box.style.border = "none";
+  });
+  this.style.borderColor = "black";
+  this.style.border = "2px solid";
+  this.style.transition = "none";
+  document.querySelector('.ostalo').style.display = 'none';
+  document.querySelector('.povrce').style.display = 'block';
+  document.querySelector('.or').style.display = 'none';
+  document.querySelector('.zac').style.display = 'none';
+});
+
+document.querySelector('.box4').addEventListener('click', function() {
+  document.querySelectorAll('.box1, .box2, .box3, .box4, .box5').forEach(box => {
+      box.style.borderColor = "transparent";
+      box.style.border = "none";
+  });
+  this.style.borderColor = "black";
+  this.style.border = "2px solid";
+  this.style.transition = "none";
+  document.querySelector('.ostalo').style.display = 'none';
+  document.querySelector('.povrce').style.display = 'none';
+  document.querySelector('.or').style.display = 'block';
+  document.querySelector('.zac').style.display = 'none';
+});
+
+document.querySelector('.box5').addEventListener('click', function() {
+  document.querySelectorAll('.box1, .box2, .box3, .box4, .box5').forEach(box => {
+      box.style.borderColor = "transparent";
+      box.style.border = "none";
+  });
+  this.style.borderColor = "black";
+  this.style.border = "2px solid";
+  this.style.transition = "none";
+  document.querySelector('.ostalo').style.display = 'none';
+  document.querySelector('.povrce').style.display = 'none';
+  document.querySelector('.or').style.display = 'none';
+  document.querySelector('.zac').style.display = 'block';
+});
+
